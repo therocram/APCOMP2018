e0c6e49beffda5ca6de689393e52029ed998bd19
Masen Pitts <19mpitts@go.dsdmail.net>
2019-02-12 08:49:46 -0700

    Adding elevens lab to git

---
+++ src/elevens/DeckTester.java
@@ -0,0 +1,31 @@
+package elevens;
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args)
+	{
+		String ranks[] = {"King", "Queen", "Jack", "Ten"};
+		int values[] = {13, 12, 11, 10};
+		String suits[] = {"Spades", "Hearts", "Clubs"};
+		
+		Deck elevens = new Deck(ranks, suits, values);
+		
+		/*for(int i = 0; i < ranks.length * suits.length; i++)
+		{
+			elevens.deal();
+		}*/
+	
+		elevens.shuffle();
+		
+		System.out.println(elevens.toString());
+		
+		
+		
+	}
+}
