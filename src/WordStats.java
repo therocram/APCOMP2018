4a02d652b26d761e99b0829d33834138121391b5
Therocram <therocram@gmail.com>
2019-03-11 20:07:52 -0600

    Finished WordStats

--- src/WordStats.java
+++ src/WordStats.java
@@ -17,47 +17,66 @@ public class WordStats
 		
 		input= input.trim();
 		
-		int words = 0, length, pos = 0, a;
+		int words = 0, length, pos = 0; //, a;
 		double avelength = 0;
-		
-		length = input.length();
-		
-		if(!input.isEmpty() && input.charAt(pos) != ' ')
-		{
-			words++;
-			a = input.indexOf(" ", pos);
-			if(a == -1)
-				avelength += length;
-			else
-				avelength += a;	
-		}
-		
-		while(true)
-		{
-			a = input.indexOf(" ", pos);
-			
-			if(a == -1)
-			{
-				break;
-			}
-			else if(input.charAt(a + 1) != ' ')
-			{
-				words++;
-				avelength += a - pos - 1;
-				System.out.println(avelength);
-			}
-			
-			pos = a + 1;
-		}
 		
 		if(!input.isEmpty())
 		{
-			avelength /= (double) words;
+			length = input.length();
+			
+			if(input.indexOf(" ") == -1)
+			{
+				words = 1;
+				avelength = length;
+			}
+			
+			else
+			{
+				String w;
+				int end = 0; 
+				
+				while(end != -1)
+				{
+					end = input.indexOf(" ", pos);
+					
+					if(end == -1)
+					{
+						if(input.charAt(end + 1) != ' ' && pos != length)
+						{
+							words++;
+							avelength += length - pos;
+						}
+						
+						break;
+					}
+					
+					if(input.charAt(end + 1) != ' ')
+					{
+						w = input.substring(pos, end);
+						
+						words++;
+						
+						avelength += w.length();
+					}
+					
+					pos = end + 1;
+				}
+				
+				avelength /= (double) words;
+			}
+						
+			
+			JOptionPane.showMessageDialog(null, "Sentence Length: " + length + "\n"
+											  + "Total Words: " + words + "\n"
+											  + "Average Word Length: " + avelength, title, 1);
 		}
 		
-		JOptionPane.showMessageDialog(null, "Sentence Length: " + length + "\n"
-										  + "Total Words: " + words + "\n"
-										  + "Average Word Length: " + avelength, title, 1);
+		else
+		{
+			JOptionPane.showMessageDialog(null, "Error: string must not be empty", "Program Exception", 0);
+		}
+		
+		
 		
 	}
 
