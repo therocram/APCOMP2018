8934dc15f02a8b4ed6791e8c3e053514f0f616d0
Masen Pitts <19mpitts@go.dsdmail.net>
2019-03-29 09:00:06 -0600

    Remaking TestScores with menus

---
+++ src/student/TSVMenu.java
@@ -0,0 +1,23 @@
+package student;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TSVMenu extends JFrame
+{
+	private TestScoresModel model;
+	
+	////////
+	
+	private JMenuItem newMI 		= new JMenuItem("New");
+	private JMenuItem openMI 		= new JMenuItem("Open");
+	private JMenuItem saveMI 		= new JMenuItem("Save");
+	private JMenuItem addMI 		= new JMenuItem("Add");
+	private JMenuItem modifyMI 		= new JMenuItem("Modify");
+	private JMenuItem deleteMI 		= new JMenuItem("Delete");
+	private JMenuItem highScoreMI 	= new JMenuItem("Highest Score");
+	private JMenuItem aveScoreMI 	= new JMenuItem("Class Average");
+	
+	////////
+	
+}
